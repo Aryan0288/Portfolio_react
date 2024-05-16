cd0ad7e04d9a9ac219b2a827d432159b1f64d85f
Aryan <aryansharma31658@gmail.com>
2024-05-16 18:43:19 +0530

    chnage in image tag

--- src/Components/home.jsx
+++ src/Components/home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import '../ComponentCss/HomeCss.css'
+import '../ComponentCss/HomeCss.css';
 import TypedText from './TypedText'
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -17,8 +17,8 @@ export default function home() {
                 <div class="container">
                     <div class="navbar">
                         <div class="logo-container">
-                            {/* <img src="../../public/imges/images_A.jpg" class="logo" alt="" /> */}
-                            <img src="../../public/imges/remove_bg.png" class="logo" alt="" />
+                            {/* <img src="imges/images_A.jpg" class="logo" alt="" /> */}
+                            <img src="imges/remove_bg.png" class="logo" alt="" />
                             <div class=" logo-text">ryan</div>
 
                         </div>
@@ -59,22 +59,22 @@ export default function home() {
 
                         <div class="hero-section-right">
                             <div class="absolute icons icon-dots">
-                                <img src="../../public/imges/dots.png" alt="" />
+                                <img src="imges/dots.png" alt="" />
                             </div>
                             <div class="absolute icons icon-cube">
-                                <img src="../../public/imges/cube.png" alt="" />
+                                <img src="imges/cube.png" alt="" />
                             </div>
                             <div class="absolute icons icon-circle">
-                                <img src="../../public/imges/circle.png" alt="" />
+                                <img src="imges/circle.png" alt="" />
                             </div>
                             <div class="absolute icons icon-zigzak">
-                                <img src="../../public/imges/zigzags.png" alt="" />
+                                <img src="imges/zigzags.png" alt="" />
                             </div>
                             <div class="absolute icons icon-plus">
-                                <img src="../../public/imges/plus.png" alt="" />
+                                <img src="imges/plus.png" alt="" />
                             </div>
                             <div class="user-image">
-                                <img src="../../public/imges/Aryan_img.png" alt="" />
+                                <img src="imges/Aryan_img.png" alt="" />
                             </div>
                         </div>
                         <div class="btn-pink" id="btn-bottom">Hire me</div>
@@ -93,13 +93,13 @@ export default function home() {
                             <div class="project-content project-content-right">
 
                                 <div class="project-skill-container">
-                                    <img class="project-skill" src="../../public/imges/HTML.png" alt="" />
-                                    <img class="project-skill" src="../../public/imges/CSS.png" alt="" />
-                                    {/* <img class="project-skill" src="../../public/imges/javascript.png" alt="" /> */}
+                                    <img class="project-skill" src="imges/HTML.png" alt="" />
+                                    <img class="project-skill" src="imges/CSS.png" alt="" />
+                                    {/* <img class="project-skill" src="imges/javascript.png" alt="" /> */}
                                     <FontAwesomeIcon className='icon' icon={faJs} />
                                     <FontAwesomeIcon className='icon' icon={faReact} />
-                                    <img class="project-skill" src="../../public/imges/Express.png" alt="" />
-                                    <img class="project-skill" src="../../public/imges/Tailwind.png" alt="" />
+                                    <img class="project-skill" src="imges/Express.png" alt="" />
+                                    <img class="project-skill" src="imges/Tailwind.png" alt="" />
                                     <img class="project-skill" src="https://codehelp-portfolio-website.netlify.app/images/stack/NodeJs.svg" alt="" />
 
                                     <img class="project-skill" src="https://codehelp-portfolio-website.netlify.app/images/stack/MongoDB.svg" alt="" />
@@ -130,11 +130,11 @@ export default function home() {
                             <div class="project-content project-content-left">
 
                                 <div class="project-skill-container">
-                                    <img class="project-skill" src="../../public/imges/HTML.png" alt="" />
-                                    <img class="project-skill" src="../../public/imges/CSS.png" alt="" />
+                                    <img class="project-skill" src="imges/HTML.png" alt="" />
+                                    <img class="project-skill" src="imges/CSS.png" alt="" />
                                     <FontAwesomeIcon className='icon' icon={faJs} />
                                     <FontAwesomeIcon className='icon' icon={faReact} />
-                                    <img class="project-skill" src="../../public/imges/Tailwind.png" alt="" />
+                                    <img class="project-skill" src="imges/Tailwind.png" alt="" />
                                     <img class="project-skill"
                                         src="https://pbs.twimg.com/profile_images/1633183038140981248/Mz4bv8Ja_400x400.png" alt="" />
                                 </div>
@@ -160,10 +160,10 @@ export default function home() {
                             <div id='special-subheading' class="project-content project-content-right ">
 
                                 <div class="project-skill-container">
-                                    <img class="project-skill" src="../../public/imges/HTML.png" alt="" />
-                                    <img class="project-skill" src="../../public/imges/CSS.png" alt="" />
+                                    <img class="project-skill" src="imges/HTML.png" alt="" />
+                                    <img class="project-skill" src="imges/CSS.png" alt="" />
                                     <FontAwesomeIcon className='icon' icon={faJs} />
-                                    <img class="project-skill" src="../../public/imges/Tailwind.png" alt="" />
+                                    <img class="project-skill" src="imges/Tailwind.png" alt="" />
                                     <img class="project-skill" src="https://pbs.twimg.com/profile_images/1633183038140981248/Mz4bv8Ja_400x400.png" alt="" />
 
                                 </div>
@@ -187,12 +187,12 @@ export default function home() {
                             <div class="project-content project-content-left">
 
                                 <div class="project-skill-container">
-                                    <img class="project-skill" src="../../public/imges/HTML.png" alt="" />
-                                    <img class="project-skill" src="../../public/imges/CSS.png" alt="" />
-                                    <img class="project-skill" src="../../public/imges/javascript.png" alt="" />
+                                    <img class="project-skill" src="imges/HTML.png" alt="" />
+                                    <img class="project-skill" src="imges/CSS.png" alt="" />
+                                    <img class="project-skill" src="imges/javascript.png" alt="" />
                                     {/* <!-- <img class="project-skill" src="Express.png" alt=""> --> */}
-                                    <img class="project-skill" src="../../public/imges/NextJsCircle.png" alt="" />
-                                    <img class="project-skill" src="../../public/imges/Tailwind.png" alt="" />
+                                    <img class="project-skill" src="imges/NextJsCircle.png" alt="" />
+                                    <img class="project-skill" src="imges/Tailwind.png" alt="" />
                                     {/* <!-- <img class="project-skill" src="https://codehelp-portfolio-website.netlify.app/images/stack/NodeJs.svg" alt=""> -->
                             <!-- <img class="project-skill" src="https://codehelp-portfolio-website.netlify.app/images/stack/MongoDB.svg" alt=""> -->
                             <!-- <img class="project-skill" src="https://codehelp-portfolio-website.netlify.app/images/stack/Redux.svg" alt=""> --> */}
@@ -254,33 +254,33 @@ export default function home() {
                     </div>
 
                     <div class="skill-container-right">
-                        <img src="../../public/imges/blob vector.png" class="blob-style" alt="" />
+                        <img src="imges/blob vector.png" class="blob-style" alt="" />
 
-                        <img src="../../public/imges/HTML.png" class="skill-logo" alt="" title="Html" />
-                        <img src="../../public/imges/CSS.png" class="skill-logo" alt="" title="Css" />
+                        <img src="imges/HTML.png" class="skill-logo" alt="" title="Html" />
+                        <img src="imges/CSS.png" class="skill-logo" alt="" title="Css" />
                         <img src="https://codehelp-portfolio-website.netlify.app/images/stack/Javascript.svg" class="skill-logo"
                             alt="" title="JavaScript" />
-                        <img src="../../public/imges/React.png" class="skill-logo" alt="" title="React" />
+                        <img src="imges/React.png" class="skill-logo" alt="" title="React" />
                         <img src="https://codehelp-portfolio-website.netlify.app/images/stack/NodeJs.svg" class="skill-logo"
                             alt="" title="NodeJs" />
 
                         <img src="https://codehelp-portfolio-website.netlify.app/images/stack/Redux.svg" class="skill-logo"
                             alt="" title="Redux" />
-                        <img src="../../public/imges/Tailwind.png" class="skill-logo" alt="" title="Tailwind" />
-                        <img src="../../public/imges/bootstrap.png" class="skill-logo" alt="" title="Bootstrap" />
-                        <img src="../../public/imges/materialUi.png" class="skill-logo" alt="" title="materialUi" />
-                        <img src="../../public/imges/Express.png" class="skill-logo" alt="" title="Express" />
-                        <img src="../../public/imges/git.png" class="skill-logo" alt="" title="git" />
+                        <img src="imges/Tailwind.png" class="skill-logo" alt="" title="Tailwind" />
+                        <img src="imges/bootstrap.png" class="skill-logo" alt="" title="Bootstrap" />
+                        <img src="imges/materialUi.png" class="skill-logo" alt="" title="materialUi" />
+                        <img src="imges/Express.png" class="skill-logo" alt="" title="Express" />
+                        <img src="imges/git.png" class="skill-logo" alt="" title="git" />
                         <img src="https://codehelp-portfolio-website.netlify.app/images/stack/Github.svg" class="skill-logo"
                             alt="" title="GitHub" />
 
                         <img src="https://codehelp-portfolio-website.netlify.app/images/stack/MongoDB.svg" class="skill-logo"
                             alt="" title="MongoDB" />
-                        <img src="../../public/imges/vercel.jpg" class="skill-logo" alt="" title="Vercel" />
-                        <img src="../../public/imges/Bash.png" class="skill-logo" alt="" title="Bash" />
-                        {/* <img src="https://codehelp-portfolio-website.netlify.app/images/stack/Docker.svg" class="skill-logo"
-                            alt="" title="Html" /> */}
-                        {/* <img src="../../public/imges/K8s.png" class="skill-logo" alt="" title="Html" /> */}
+                        <img src="imges/vercel.jpg" class="skill-logo" alt="" title="Vercel" />
+                        <img src="imges/Bash.png" class="skill-logo" alt="" title="Bash" />
+                        <img src="https://codehelp-portfolio-website.netlify.app/images/stack/Docker.svg" class="skill-logo"
+                            alt="" title="Docker" />
+                        {/* <img src="imges/K8s.png" class="skill-logo" alt="" title="Html" /> */}
                         <img src="https://www.stonebranch.com/integration-hub/media/3c/64/66/1636642258/Stonebranch_SQL_Vendor_Product_Logo.svg" class="skill-logo" alt="" title="Sql" />
                         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROFNi8oEe3Wd_duKIjeHPLebMrC-3fB5vlqr9fn8y2oOcX9_uEAP3j9mFQGA5u-5s2DEw&usqp=CAU" class="skill-logo" alt="" title="postman" />
                         <img src="https://yt3.googleusercontent.com/_q52i8bUAEvcb7JR4e-eNTv23y2A_wg5sCz0NC0GrGtcw1CRMWJSOPVHUDh_bngD0q4gMvVeoA=s900-c-k-c0x00ffffff-no-rj" class="skill-logo" alt="" title="vsCode" />
